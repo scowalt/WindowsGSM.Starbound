de668d931b4139f97a34736b8c345bfe6da72d57
Scott Walters <me@scowalt.com>
2020-12-03 18:45:00 -0800

    Improvements, appears to have basic functionality

--- Starbound.cs/Starbound.cs
+++ Starbound.cs/Starbound.cs
@@ -1,12 +1,8 @@
-using System;
 using System.Diagnostics;
-using System.IO;
-using System.Net;
-using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using WindowsGSM.Functions;
 using WindowsGSM.GameServer.Engine;
-using WindowsGSM.GameServer.Query;
 
 namespace WindowsGSM.Plugins
 {
@@ -16,17 +12,17 @@ namespace WindowsGSM.Plugins
 		{
 			name = "WindowsGSM.Starbound",
 			author = "ScoWalt",
-			description = "🧩 WindowsGSM plugin for Starbound Dedicated Server"
+			description = "🧩 WindowsGSM plugin for Starbound Dedicated Server",
 			version = "0.1",
 			url = "https://github.com/scowalt/WindowsGSM.Starbound",
 			color = "#221868"
-		}
+		};
 
 		public Starbound(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
 		private readonly ServerConfig _serverData;
 
 		// Properties for SteamCMD installer
-		public override bool loginAnonymous => true; // Starbound Dedicated Server does NOT allow anonymous login. See https://developer.valvesoftware.com/wiki/Dedicated_Servers_List
+		public override bool loginAnonymous => false; // Starbound Dedicated Server does NOT allow anonymous login. See https://developer.valvesoftware.com/wiki/Dedicated_Servers_List
 		public override string AppId => "533830"; // https://steamdb.info/app/533830/
 
 		// Standard variables
@@ -38,12 +34,12 @@ namespace WindowsGSM.Plugins
 
 		public string Port = "21025"; // Default port
 		public string QueryPort = "21025"; // Default Query Port
-		public string Defaultmap = "empty"; // Default map name (arbitrary for Starbound)
+		public string Defaultmap = "base"; // Default map name (arbitrary for Starbound)
 		public string Maxplayers = "8"; // Default maxplayers
 		public string Additional = ""; // Additional server start parameter
 
 		// Standard functions
-		public async void CreateServerCFG() { /* Starbound creates its own default server config on first launch */ }
+		public async void CreateServerCFG() { /* Starbound creates its own default server config on first launch */}
 		public async Task<Process> Start()
 		{
 			var p = new Process
@@ -52,21 +48,21 @@ namespace WindowsGSM.Plugins
 				{
 					WindowStyle = ProcessWindowStyle.Minimized,
 					UseShellExecute = false,
-					WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+					WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID, "win64"),
 					FileName = ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath),
 					Arguments = ""
 				},
 				EnableRaisingEvents = true
-			}
+			};
 
 			try
 			{
 				p.Start();
 				return p;
 			}
-			catch (Exception)
+			catch (System.Exception e)
 			{
-				base.Error = e.message;
+				base.Error = e.Message;
 				return null;
 			}
 		}
